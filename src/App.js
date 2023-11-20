95d36ce47c15a32d283bb19e1a81580b8dbaf7c7
Ashutosh741 <106524453+Ashutosh741@users.noreply.github.com>
2023-11-20 17:02:24 +0530

    returning only 404 page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ErrorPage from "./Components/Pages/404Page";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="font-gilroy font-medium text-gray text-lg leading-[27px]">
-        <Routes>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <div className="font-gilroy font-medium text-gray text-lg leading-[27px]">
+    //     <Routes>
+    //       <Route path="*" element={<ErrorPage />} />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
+    <ErrorPage/>
   );
 }
 
