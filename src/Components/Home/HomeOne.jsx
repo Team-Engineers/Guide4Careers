5f2816abe5957ee859f141d821ae15edf0fae764
ditya17 <nitya17nitya@gmail.com>
2023-12-14 22:11:40 +0530

    footer-changes

--- src/Components/Home/HomeOne.jsx
+++ src/Components/Home/HomeOne.jsx
@@ -1,26 +1,19 @@
 import React, { useEffect } from "react";
-// import About from "../About";
+import Assessment from "../Assessment/Assessment";
 import Banner from "../Banner";
 import Blogs from "../Blogs";
-// import Brands from "../Brands";
-// import Courses from "../Courses";
+import Categories from "../Categories/Categories";
+import Consultant from "../Consultant";
+import Exam from "../Exam/Exam";
 import Feature from "../Feature";
 import Footer from "../Footer";
-import Header from "../Header";
-// import NewsLetter from "../NewsLetter";
-// import Platform from "../Platform";
-// import Team from "../Team";
-import Testimonials from "../Testimonials";
-// import Topics from "../Topics";
-import TopColleges from "../TopColleges";
-import OurPartnerInstitutions from "../OurPartnerInstitutions";
-import Oursuccess from "../Oursuccess/Oursuccess";
 import Founder from "../Founder";
-import Consultant from "../Consultant";
-import Assessment from "../Assessment/Assessment";
+import Header from "../Header";
+import OurPartnerInstitutions from "../OurPartnerInstitutions";
 import Ourservices from "../Ourservices/Ourservices";
-import Categories from "../Categories/Categories";
-import Exam from "../Exam/Exam";
+import Oursuccess from "../Oursuccess/Oursuccess";
+import Testimonials from "../Testimonials";
+import TopColleges from "../TopColleges";
 
 
 
@@ -29,16 +22,14 @@ const HomeOne = () => {
     document.documentElement.scrollTop = 0;
   }, []);
   return (
+<<<<<<< Updated upstream
     <div className=" overflow-hidden ">
+=======
+    <div className="overflow-hidden">
+>>>>>>> Stashed changes
       <Header />
       <Banner />
-      {/* <About /> */}
-      {/* <Brands section_padding_bottom={"section-padding-bottom"} /> */}
       <Feature />
-      {/* <Courses />*/}
-      {/*<Platform />*/}
-      {/* <NewsLetter />*/}
-      {/* <Team /> */}
       <TopColleges />
       <OurPartnerInstitutions />
       <Oursuccess/>
@@ -48,9 +39,6 @@ const HomeOne = () => {
       <Ourservices/>
       <Categories/>
       <Exam/>
-      
-      {/* <Topics /> */}
-
       <Testimonials />
       <Blogs />
       <Footer />
